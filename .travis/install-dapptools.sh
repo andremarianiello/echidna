813006f37cc5bb795da10cd43eb09cec1f82038c
Ben Perez <benperez1227@gmail.com>
2018-08-30 16:11:31 -0400

    Builds with cachix, but requires running build.sh and then stack install

---
+++ .travis/install-dapptools.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eux
+
+wget https://github.com/dapphub/dapptools/archive/master.zip
+unzip master.zip
+mv dapptools-master dapptools
+cd dapptools
+cd src/libethjet
+cachix use dapp
+sed -i.bak "s/{ stdenv, secp256k1 }:/with import <nixpkgs> {};/" default.nix
+nix-env -i secp256k1
+nix-env -f . -i libethjet
---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -eux
+
+NIX=`which nix-env 2>/dev/null`
+
+if [ "$NIX" = "" ]; then
+  echo "Error: can't find 'nix-env' in your \$PATH" 1>&2
+  exit 1
+fi
+
+wget https://github.com/dapphub/dapptools/archive/master.zip
+unzip master.zip
+mv dapptools-master dapptools
+cd dapptools
+cd src/libethjet
+cachix use dapp
+sed -i.bak "s/{ stdenv, secp256k1 }:/with import <nixpkgs> {};/" default.nix
+nix-env -i secp256k1
+nix-env -f . -i libethjet
+cd ../../..
