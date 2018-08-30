385c568c85519060ea890474a463b182460bc949
Ben Perez <benperez1227@gmail.com>
2018-08-30 17:12:44 -0400

    Permissions issue with OSX

--- .travis/install-dapptools.sh
+++ .travis/install-dapptools.sh
@@ -8,6 +8,7 @@ mv dapptools-master dapptools
 cd dapptools
 cd src/libethjet
 nix-env -iA cachix -f https://github.com/NixOS/nixpkgs/tarball/1d4de0d552ae9aa66a5b8dee5fb0650a4372d148
+echo \"trusted-users = root $USER\" | sudo tee -a /etc/nix/nix.conf && sudo pkill nix-daemon
 cachix use dapp
 sed -i.bak "s/{ stdenv, secp256k1 }:/with import <nixpkgs> {};/" default.nix
 nix-env -i secp256k1
