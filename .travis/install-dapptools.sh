7e94c82790655367997aa261c46b3d2fc8a00c86
Ben Perez <benperez1227@gmail.com>
2018-08-30 16:33:21 -0400

    Install cachix during travis build

--- .travis/install-dapptools.sh
+++ .travis/install-dapptools.sh
@@ -7,6 +7,7 @@ unzip master.zip
 mv dapptools-master dapptools
 cd dapptools
 cd src/libethjet
+nix-env -iA cachix -f https://github.com/NixOS/nixpkgs/tarball/1d4de0d552ae9aa66a5b8dee5fb0650a4372d148
 cachix use dapp
 sed -i.bak "s/{ stdenv, secp256k1 }:/with import <nixpkgs> {};/" default.nix
 nix-env -i secp256k1
